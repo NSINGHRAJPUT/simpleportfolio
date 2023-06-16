ef7bb2ed0b7ec5ec9b6798b41f5b83bcca9aa4b5
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-06-16 21:53:12 +0530

    changes

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -2,28 +2,28 @@ import  './Education.css';
 
 const Education = () =>{
     return <section className='education'>
-        <div className='classx animate__animated animate__fadeInLeft animate__delay-1s'>
-            <h2 className='classname'>Class X</h2>
-            <h3 className='classyear'>(2012-2013)</h3>
-            <h3 className='classsubject'>Computer Science</h3>
-            <h3 className='classinstitute'>Gyan Ganga International Academy, Ratanpur, Bhopal (M.P.)</h3>
-            <h3 className='classaff'>CBSE</h3>
+        <div className='classx '>
+            <h2 className='classname animate__animated animate__fadeInRight animate__delay-1s'>Class X</h2>
+            <h3 className='classyear animate__animated animate__fadeInLeft animate__delay-1s'>(2012-2013)</h3>
+            <h3 className='classsubject animate__animated animate__fadeInRight animate__delay-1s'>Computer Science</h3>
+            <h3 className='classinstitute animate__animated animate__fadeInLeft animate__delay-1s'>Gyan Ganga International Academy, Ratanpur, Bhopal (M.P.)</h3>
+            <h3 className='classaff animate__animated animate__fadeInRight animate__delay-1s'>CBSE</h3>
         </div>
-        <div className='classx animate__animated animate__fadeInRight animate__delay-1s'>
-            <h2 className='classname'>Class XII</h2>
+        <div className='classx '>
+            <h2 className='classname animate__animated animate__fadeInRight animate__delay-1s'>Class XII</h2>
             <h3 className='classyear'>(2014-2015)</h3>
-            <h3 className='classsubject'>Computer Science</h3>
+            <h3 className='classsubject animate__animated animate__fadeInRight animate__delay-1s'>Computer Science</h3>
             <h3 className='classinstitute'>Gyan Ganga International Academy, Ratanpur, Bhopal (M.P.)</h3>
-            <h3 className='classaff'>CBSE</h3>
+            <h3 className='classaff animate__animated animate__fadeInRight animate__delay-1s'>CBSE</h3>
         </div>
-        <div className='classx animate__animated animate__fadeInLeft animate__delay-1s'>
+        <div className='classx'>
             <h2 className='classname'>B.Sc</h2>
             <h3 className='classyear'>(2015-2018)</h3>
             <h3 className='classsubject'>Computer Science</h3>
             <h3 className='classinstitute'>CPS college Badagaon, Morena (M.P.)</h3>
             <h3 className='classaff'>Jiwaji University</h3>
         </div>
-        <div className='classx animate__animated animate__fadeInRight animate__delay-1s'>
+        <div className='classx '>
             <h2 className='classname'>B.Ed</h2>
             <h3 className='classyear'>(2019-2021)</h3>
             <h3 className='classinstitute'>SSBBSMP, Morena (M.P.)</h3>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,7 @@ const Hero = () =>{
             <h1 className="animate__animated animate__backInUp animate__delay-1s"> Hi, I am <br/>NEERAJ SINGH RAJPUT<br/></h1>
             <h2 className='job_title animate__animated animate__backInDown animate__delay-1s'>Front End Developer</h2>
             <p className='intro animate__animated animate__backInLeft animate__delay-1s'>A creative Front End Web Developer, and I'm very passionate to my work.I am skilled
-                in React and equipped with a solid coding foundation. Eager tocontribute my quick 
+                in React and equipped with a solid coding foundation. Eager to contribute my quick 
                 learning abilites and innovative mindset to a frontend web developement role</p>
             <div className='cv'>
                 <a href='#'><button>Download CV</button></a>
