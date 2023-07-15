7d6da84b52a04e5e88041bba014809fd89fab353
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-07-15 10:16:55 +0530

    background changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import './App.css';
-import solar from './assets/solar.mp4';
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import Contact from './components/Contact/Contact';
-import Projects from './components/Projects/Projects';
-import Skills from './components/Skills/Skills';
-import Education from './components/Education/Education';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import solar from "./assets/solar.mp4";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Contact from "./components/Contact/Contact";
+import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
+import Education from "./components/Education/Education";
+import Footer from "./components/Footer/Footer";
+import image from "./assets/back.png";
 
 function App() {
   return (
-    <div>
-      <video autoPlay loop muted>
-         <source src={solar} type="video/mp4" />
-      </video>
-      <Header/>
-      <Hero/>
-      <h2 className='projects'>Education&nbsp;</h2>
-      <Education/>
-      <h2 className='projects'>SKILLS&nbsp;</h2>
-      <Skills/>
-      <h2 className='projects'>PROJECTS&nbsp;</h2>
-      <Projects/>
-      <h2 className='projects'>CONTACT ME&nbsp;</h2>
-      <Contact/>
-      <Footer/>
+    <div className="back">
+      <img src={image} alt="" className="back-img" />
+      <Header />
+      <Hero />
+      <h2 className="projects">Education&nbsp;</h2>
+      <Education />
+      <h2 className="projects">SKILLS&nbsp;</h2>
+      <Skills />
+      <h2 className="projects">PROJECTS&nbsp;</h2>
+      <Projects />
+      <h2 className="projects">CONTACT ME&nbsp;</h2>
+      <Contact />
+      <Footer />
     </div>
   );
 }
